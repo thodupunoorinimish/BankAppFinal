a00f32957eb9934a4b7fc8a2edeba48af0a739cf
Navaneeth Nivol <navaneethsai1999@gmail.com>
2022-02-04 11:36:53 +0530

    cors fix

--- src/main/java/com/bankapp/app/controller/ResetController.java
+++ src/main/java/com/bankapp/app/controller/ResetController.java
@@ -6,13 +6,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bankapp.app.service.ResetService;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class ResetController {
 
